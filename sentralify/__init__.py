d49fc0f5e5753f365b2ed74ca801750a2834a73c
mario872 <jamesaglynn10@gmail.com>
2026-06-06 13:22:37 +1000

    Update copyright year

--- sentralify/__init__.py
+++ sentralify/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2024  James Glynn
+Copyright (C) 2026  James Glynn
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
